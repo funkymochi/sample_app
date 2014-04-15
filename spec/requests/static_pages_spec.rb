d18ecfa025a808501d3459936c1a42dc0d8faad1
Philippe Abraham <philippe.abraham@gmail.com>
2014-04-15 10:43:30 +0800

    modified gemfile rspec 2.9.0

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Static pages" do
   describe "Home page" do
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit '/static_pages/home.html.erb'
       page.should have_content ('Sample App')
     end
   end
